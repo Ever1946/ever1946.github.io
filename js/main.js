27b0d4c61b646c68a1364b4ea6730cf46b2a0b71
Ever1946 <ever150117@gmail.com>
2021-07-01 15:21:52 -0500

    first july

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var app = document.getElementById('machine');
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 60,
+});
+
+typewriter
+  .pauseFor(2300)
+  .typeString('Hola, yo soy <span class="text-warning">Ever Caicedo </span>')
+  .pauseFor(700)
+  .deleteChars(13)
+  .typeString('un Desorrallador Junior y un <strong>GEEK </strong> algo basico')
+  .pauseFor(1000)
+  .start();
